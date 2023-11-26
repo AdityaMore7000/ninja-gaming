70f39054f3d3c88fc920fe9109c96024582b2e2c
adityamore7000 <adi.more7000@gmail.com>
2023-11-26 17:39:49 +0530

    complete project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+  </script>
+  <svelte:head>
+    <title>Aditya More's Blog</title>
+  </svelte:head>
+  <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class=" w-[50vw] sm:w-[90vw] mx-auto my-[44px]">
+    <div class="mx-auto w-fit text-center">
+        <img class="mx-auto rounded-full aspect-square h-[144px]" src="https://avatars.githubusercontent.com/u/99179330?v=4" alt="">
+        <h3 class=" mt-5 text-5xl font-extrabold">Aditya More</h3>
+    </div>
+</section>
+
+<section class="md:w-[50vw] w-[80vw] mx-auto my-[54px]">
+    <div class="mx-auto w-fit sm:px-[1rem]">
+        <h6 class="text-xl">Hello, I'm <strong>Aditya</strong>, a pre-final year Student at PICT and a web development enthusiast. You can connect with me on <a class="text-blue-600 hover:underline" href="https://www.linkedin.com/in/aditya-more-9a095422a/" target="_blank">LinkedIn</a></h6>
+    </div>
+</section>
+
+<section class="md:w-[50vw] w-[80vw] mx-auto my-[54px]">
+    <div class="w-fit sm:px-[1rem] text-left">
+        <h1 class="text-3xl font-bold mb-2">Blog</h1>
+        <div class="flex flex-col py-3 gap-5">
+            <div>
+                <a href="/posts/ssg-ssr">
+                    <h1 class="text-blue-600 hover:underline text-xl">When to Use Static Generation v.s. Server-side Rendering</h1>
+                </a>
+                <p class="text-gray-500 my-1">November 26, 2023</p>
+            </div>
+            <div>
+                <a href="/posts/pre-rendering">
+                    <h1 class="text-blue-600 hover:underline text-xl">Two Forms of Pre-rendering</h1>
+                </a>
+                <p class="text-gray-500 my-1">November 26, 2023</p>
+            </div>
+            </div>
+    </div>
+</section>
